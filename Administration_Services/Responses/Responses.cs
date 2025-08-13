e803db5239701b1bc1e441af6044dda9b1f29780
João Marcelo <joaovictormarcelo5@gmail.com>
2025-08-13 20:43:47 -0300

    adding page responses and Tests in projects

--- Administration_Services/Responses/Responses.cs
+++ Administration_Services/Responses/Responses.cs
@@ -4,5 +4,26 @@ namespace Administration_Services.Responses;
 
 public class Responses<TData>
 {
-  
+    
+    [JsonConstructor]
+    public Responses (){}
+    
+ 
+    public Responses(string? message, TData data, int code = 200)
+    {
+        Code = code;
+        Message = message;
+        Data = data;
+    }
+
+    public static Responses<TData> Error(string message, TData data, int code = 400)
+        => new Responses<TData>(message, data, default);
+            
+    public int Code { get; set; }
+    public string? Message { get; set; }
+    public  TData? Data { get; set; }
+
+
+    [JsonIgnore] public bool ISsucess => Code is >= 200 and <= 299;
+    [JsonIgnore] public bool IsError => Code is >= 400 and <= 599;
 }
